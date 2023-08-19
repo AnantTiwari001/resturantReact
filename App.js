46215d73e81d75648c373ab32e7c2a2e03c9e1fe
kali0Charan <ananttiwariali@gmail.com>
2023-08-19 15:15:22 +0545

    initial commit

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import FirstPage from './Pages/FirstPage';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <MainNavigator />
+      </NavigationContainer>
   );
+}
+
+const MainStack = createStackNavigator();
+const MainNavigator = () => {
+  return (
+    <MainStack.Navigator screenOptions={{ headerShown: false }} >
+      <MainStack.Screen name='first-page' component={FirstPage} />
+    </MainStack.Navigator>
+  )
 }
 
 const styles = StyleSheet.create({
